5751f82190a87da9ff33223216ff296c766d13e9
Farithem <farithem@192.168.1.21>
2026-04-14 17:33:07 -0600

    Segundo commit

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/farithem/godesde0/variables"
 )
 
 func main() {
 	variables.MuestroEnteros()
+	variables.RestoVariables()
+	estado, texto := variables.ConviertoaTexto(1584)
+	fmt.Println("Estado:", estado)
+	fmt.Println("Texto:", texto)
 }
