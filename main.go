033b46f12e4e23ad6feea230e55a2e17493dc340
Farithem <farithem@192.168.1.21>
2026-04-14 17:51:45 -0600

    Tercer commit

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/farithem/godesde0/variables"
+	"runtime"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	/*variables.MuestroEnteros()
 	variables.RestoVariables()
 	estado, texto := variables.ConviertoaTexto(1584)
 	fmt.Println("Estado:", estado)
 	fmt.Println("Texto:", texto)
+	*/
+	if os := runtime.GOOS; os == "linux" || os == "darwin" {
+		fmt.Println("Estas usando Linux")
+		procesador := runtime.GOARCH
+		fmt.Println("Distribución:", procesador)
+	} else {
+		fmt.Println("Estás usando Windows")
+		fmt.Println(os)
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Estas usando Linux")
+	case "darwin":
+		fmt.Println("Estas usando MacOS")
+	default:
+		fmt.Printf("%s no es Linux ni MacOS", os)
+	}
 }
